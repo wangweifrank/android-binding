cd6ebf72e14b2de285bc3a48bc92dd7f5fe621e1
gueei.com <gueei.com@gmail.com>
2011-06-25 04:57:24 +0000

    Finally! V0.21 is back to Trunk!

---
+++ AndroidBinding/src/gueei/binding/Utility.java
@@ -0,0 +1,43 @@
+package gueei.binding;
+
+import java.lang.reflect.Field;
+
+import android.content.Context;
+
+public class Utility {
+	public static Object getFieldForModel(String fieldName, Object model){
+		try{
+			if (model instanceof IPropertyContainer){
+				return ((IPropertyContainer)model).getValueByName(fieldName);
+			}
+			Field field = model.getClass().getField(fieldName);
+			return field.get(model);
+		}catch(Exception e){
+			return null;
+		}
+	}
+	
+	public static IObservable<?> getObservableForModel(Context context, String fieldName, Object model){
+		return BindingSyntaxResolver.constructObservableFromStatement(context, fieldName, model);
+	}
+
+	public static Command getCommandForModel(String fieldName, Object model){
+		if (model instanceof IPropertyContainer){
+			try{
+				return ((IPropertyContainer)model).getCommandByName(fieldName);
+			}catch(Exception e){
+				return null;
+			}
+		}
+		Object rawField = getFieldForModel(fieldName, model);
+		if (rawField instanceof Command)
+			return (Command)rawField;
+		return null;
+	}
+	
+	public static int resolveResource(String attrValue, Context context){
+		if (!attrValue.startsWith("@")) return -1;
+		String name = attrValue.substring(1); // remove the @ sign
+		return context.getResources().getIdentifier(name, "layout", context.getPackageName());
+	}
+}

be8eac9c8c98c37705934f726a3219b1b5b4788f
gueei.com <gueei.com@gmail.com>
2011-06-28 01:29:06 +0000

    Added Demo for Seekbar. Increased the max of progress bar to 10000 for greater precision

--- AndroidBinding/src/gueei/binding/viewAttributes/SecondaryProgressViewAttribute.java
+++ AndroidBinding/src/gueei/binding/viewAttributes/SecondaryProgressViewAttribute.java
@@ -5,22 +5,24 @@ import android.widget.ProgressBar;
 
 
 public class SecondaryProgressViewAttribute extends ViewAttribute<ProgressBar, Float> {
+	private static final int PROGRESS_MAX = 10000;
+	
 	public SecondaryProgressViewAttribute(ProgressBar view) {
 		super(Float.class, view, "progress");
 		getView().setSecondaryProgress(0);
-		getView().setMax(1000);
+		getView().setMax(PROGRESS_MAX);
 	}
 
 	@Override
 	protected void doSetAttributeValue(Object newValue) {
 		if (newValue == null) return;
 		if (newValue instanceof Float){
-			getView().setSecondaryProgress((int)Math.ceil((Float)newValue * 1000));
+			getView().setSecondaryProgress((int)Math.ceil((Float)newValue * PROGRESS_MAX));
 		}
 	}
 
 	@Override
 	public Float get() {
-		return (float)getView().getSecondaryProgress() / 1000f;
+		return (float)getView().getSecondaryProgress() / (float)PROGRESS_MAX;
 	}
 }

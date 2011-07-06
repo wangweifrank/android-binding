04a51c6c339463ef6a3b1b2293231c85cd5fbaa7
gueei.com@gmail.com <gueei.com@gmail.com@2838e9db-b119-15c4-5bde-0ef843f85f1f>
2011-07-06 07:47:53 +0000

    Refactor on Binder, Binding Providers etc, remove redundant codes, simplifies the binding process. Added Support to Child Click in ExpandableListView and ClickedChild. ResourceLinking support

---
+++ tags/v0.3a/AndroidBinding/src/gueei/binding/IBindableView.java
@@ -0,0 +1,26 @@
+package gueei.binding;
+
+import android.view.View;
+
+/*
+ * IBindableView is the interface that supports Custom Views
+ * (For views that is not supported in Android-Binding)
+ * For any custom view, you should implement this interface 
+ * in order for the Attribute Binder to recognize it.
+ * 
+ * By Design, the Attribute Binder would put custom ViewAttributes to higher
+ * precedence, for example, if you want to override the default behavior
+ * of the "visibility" tag, you can capture that value and provide another ViewAttribute
+ * associated with that tag. In this case, Attribute Binder will stop looking for any 
+ * other "parent" to create this attribute. 
+ */
+public interface IBindableView<T extends View & IBindableView<T>> {
+	/**
+	 * The view class is recommended to create their View Attributes
+	 * If you want to override the default behavior of other ViewAttributes, you can return it here
+	 * or else, returning null will pass the control to super classes' implementation
+	 * @param attributeName
+	 * @return the ViewAttribute, or null if don't want to handle
+	 */
+	public ViewAttribute<T, ?> getViewAttribute(String attributeId);
+}
---
+++ tags/v0.3a/AndroidBinding/src/gueei/binding/collections/ExpandableCollectionAdapter.java
@@ -0,0 +1,109 @@
+package gueei.binding.collections;
+
+import gueei.binding.IObservable;
+import gueei.binding.viewAttributes.templates.Layout;
+
+import java.util.WeakHashMap;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.BaseExpandableListAdapter;
+
+
+public class ExpandableCollectionAdapter extends BaseExpandableListAdapter{
+	private final String mChildName;
+	private final Layout mChildLayout;
+	private final WeakHashMap<Integer, Adapter> mChildAdapters =
+		new WeakHashMap<Integer, Adapter>();
+	private final Adapter mGroupAdapter;
+	private final Context mContext;
+
+	public ExpandableCollectionAdapter(Context context, Adapter groupAdapter, String childName, Layout childLayout){
+		mChildName = childName;
+		mChildLayout = childLayout;
+		mContext = context;
+		mGroupAdapter = groupAdapter;
+	}
+	/*
+	public ExpandableCollectionAdapter(Context context,
+			IObservableCollection<?> collection, int layoutId, int dropDownLayoutId, String childName, int childLayoutId)
+			throws Exception {
+		mChildName = childName;
+		mChildLayoutId = childLayoutId;
+		mContext = context;
+		mGroupAdapter = new CollectionAdapter(context, collection, layoutId, dropDownLayoutId);
+	}
+*/
+	
+	private Adapter getChildAdapter(int groupPosition){
+		Log.d("Binder", "Get Child Adapter " + groupPosition);
+		try{
+			if (!mChildAdapters.containsKey(groupPosition)){
+				Object item = mGroupAdapter.getItem(groupPosition);
+				if (item instanceof LazyLoadParent){
+					((LazyLoadParent)item).onLoadChildren();
+				}
+				IObservable<?> child = 
+					gueei.binding.Utility.getObservableForModel(mContext, mChildName, item);
+				mChildAdapters.put(groupPosition, 
+					Utility.getSimpleAdapter(mContext, child.get(), mChildLayout, mChildLayout));
+			}
+			return mChildAdapters.get(groupPosition);
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public Object getChild(int groupPosition, int childPosition) {
+		return getChildAdapter(groupPosition).getItem(childPosition);
+	}
+
+	public long getChildId(int groupPosition, int childPosition) {
+		return getChildAdapter(groupPosition).getItemId(childPosition);
+	}
+
+	public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView,
+			ViewGroup parent) {
+		return getChildAdapter(groupPosition).getView(childPosition, convertView, parent);
+	}
+
+	public int getChildrenCount(int groupPosition) {
+		return getChildAdapter(groupPosition).getCount();
+	}
+
+	public Object getGroup(int groupPosition) {
+		return mGroupAdapter.getItem(groupPosition);
+	}
+
+	public int getGroupCount() {
+		return mGroupAdapter.getCount();
+	}
+
+	public long getGroupId(int groupPosition) {
+		return mGroupAdapter.getItemId(groupPosition);
+	}
+
+	public View getGroupView(int groupPosition, boolean isExpanded,
+			View convertView, ViewGroup parent) {
+		return mGroupAdapter.getView(groupPosition, convertView, parent);
+	}
+
+	public boolean isChildSelectable(int groupPosition, int childPosition) {
+		return true;
+	}
+
+	public void onGroupCollapsed(int groupPosition) {
+		mChildAdapters.remove(groupPosition);
+	}
+
+	public void onGroupExpanded(int groupPosition) {
+	}
+
+	public boolean hasStableIds() {
+		return true;
+	}
+}

00ed9e80e276eb578a2b5ccb11a13499f9f94d66
kulynyak@gmail.com <kulynyak@gmail.com@2838e9db-b119-15c4-5bde-0ef843f85f1f>
2011-08-10 12:30:01 +0000

    Made TabHost working with AB.

--- branches/v0.4/AndroidBinding/src/gueei/binding/viewAttributes/tabHost/TabsViewAttribute.java
+++ branches/v0.4/AndroidBinding/src/gueei/binding/viewAttributes/tabHost/TabsViewAttribute.java
@@ -12,62 +12,70 @@ import gueei.binding.collections.ArrayListObservable;
 
 @SuppressWarnings("rawtypes")
 public class TabsViewAttribute extends ViewAttribute<TabHost, ArrayListObservable> {
+
 	public TabsViewAttribute(TabHost view) {
 		super(ArrayListObservable.class, view, "tabs");
 	}
 
 	private ArrayListObservable<Tab> mTabs;
-	
-	@SuppressWarnings("unchecked")
-	@Override
+
+	@SuppressWarnings("unchecked") @Override
 	protected void doSetAttributeValue(Object newValue) {
-		if (!(newValue instanceof ArrayListObservable)) return;
-		
-		// Type is not Tab
-		if (!Tab.class.isAssignableFrom(((ArrayListObservable)newValue).getComponentType()))
+		if (!(newValue instanceof ArrayListObservable)) {
 			return;
-		
-		mTabs = (ArrayListObservable<Tab>)newValue;
-		
-		for(Tab t: mTabs){
+		}
+		// Type is not Tab
+		if (!Tab.class.isAssignableFrom(((ArrayListObservable) newValue).getComponentType())) {
+			return;
+		}
+		mTabs = (ArrayListObservable<Tab>) newValue;
+		for (Tab t : mTabs) {
 			mTabHost.addTab(constructTabSpec(t));
 		}
 	}
-	
-	private TabSpec constructTabSpec(Tab tab){
+
+	private TabSpec constructTabSpec(Tab tab) {
 		TabSpec spec = mTabHost.newTabSpec(tab.Tag.get());
-		if (tab.Icon.get() != null){
+		if (tab.Icon.get() != null) {
 			spec.setIndicator(tab.Label.get(), tab.Icon.get());
-		}else{
+		}
+		else {
 			spec.setIndicator(tab.Label.get());
 		}
-		if (tab.Activity.get() != null){
+		if (tab.Activity.get() != null) {
 			Intent intent;
 			try {
 				intent = new Intent(getView().getContext(), Class.forName(tab.Activity.get()));
 				spec.setContent(intent);
-			} catch (ClassNotFoundException e) {
+			}
+			catch (ClassNotFoundException e) {
 				e.printStackTrace();
 			}
-		}else{
+		}
+		else {
 			spec.setContent(tab.ViewId.get());
 		}
-		
 		return spec;
 	}
 
 	TabHost mTabHost;
-	
+
 	@Override
-	protected void onBind(Context context, IObservable<?> prop,
-			BindingType binding) {
-		if (!(context instanceof TabActivity)) return;
-		mTabHost = ((TabActivity)context).getTabHost();
+	protected void onBind(Context context, IObservable<?> prop, BindingType binding) {
+		if (!(context instanceof TabActivity)) {
+			return;
+		}
+		mTabHost = ((TabActivity) context).getTabHost();
 		super.onBind(context, prop, binding);
 	}
 
 	@Override
 	public ArrayListObservable<Tab> get() {
 		return mTabs;
+	}
+
+	@Override
+	protected BindingType AcceptThisTypeAs(Class<?> type) {
+		return BindingType.OneWay;
 	}
 }

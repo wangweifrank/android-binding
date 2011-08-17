4527c5f802c4a878e1e46a0883ff1a9e965e2f93
kulynyak <kulynyak@gmail.com>
2011-08-17 15:53:29 +0000

    More Tab support - tab Changed Command

--- AndroidBinding/src/gueei/binding/viewAttributes/tabHost/TabSelectedPositionViewAttribute.java
+++ AndroidBinding/src/gueei/binding/viewAttributes/tabHost/TabSelectedPositionViewAttribute.java
@@ -2,39 +2,47 @@ package gueei.binding.viewAttributes.tabHost;
 
 import android.view.View;
 import android.widget.TabHost;
+import gueei.binding.Binder;
 import gueei.binding.BindingType;
 import gueei.binding.ViewAttribute;
+import gueei.binding.listeners.OnTabChangedListener;
 
 /**
  * User: =ra=
  * Date: 16.08.11
  * Time: 20:46
  */
-public class TabSelectedPositionViewAttribute extends ViewAttribute<View, Integer> {
+public class TabSelectedPositionViewAttribute extends ViewAttribute<View, Integer> implements TabHost.OnTabChangeListener {
 
-	private int mTabSelectedPosition = -1;
 	public TabSelectedPositionViewAttribute(View view) {
 		super(Integer.class, view, "selectedPosition");
+		Binder.getMulticastListenerForView(view, OnTabChangedListener.class).register(this);
 	}
 
-	@Override
-	protected void doSetAttributeValue(Object newValue) {
-		if (newValue == null) {
+	private int mOldValue = -1;
+	private int mValue    = -1;
+
+	public void onTabChanged(String tabId) {
+		mOldValue = ((TabHost) getView()).getCurrentTab();
+		if (mOldValue != mValue) {
+			mValue = mOldValue;
+			notifyChanged(TabSelectedPositionViewAttribute.this);
+		}
+	}
+
+	@Override protected void doSetAttributeValue(Object newValue) {
+		if (!(newValue instanceof Integer) || (Integer) newValue == mValue) {
 			return;
 		}
-		if (newValue instanceof Integer) {
-			mTabSelectedPosition= (Integer)newValue;
-			((TabHost)getView()).setCurrentTab(mTabSelectedPosition);
-		}
+		mValue = (Integer) newValue;
+		((TabHost) getView()).setCurrentTab(mValue);
 	}
 
-	@Override
-	protected BindingType AcceptThisTypeAs(Class<?> type) {
+	@Override public Integer get() {
+		return mValue;
+	}
+
+	@Override protected BindingType AcceptThisTypeAs(Class<?> type) {
 		return BindingType.TwoWay;
-	}
-
-	@Override
-	public Integer get() {
-		return mTabSelectedPosition;
 	}
 }

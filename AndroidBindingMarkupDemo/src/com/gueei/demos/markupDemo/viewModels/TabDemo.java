25472be81f3bb961cc0852286960d6b8f58826b9
kulynyak <kulynyak@gmail.com>
2011-08-17 15:54:02 +0000

    More Tab support - tab Changed Command

--- AndroidBindingMarkupDemo/src/com/gueei/demos/markupDemo/viewModels/TabDemo.java
+++ AndroidBindingMarkupDemo/src/com/gueei/demos/markupDemo/viewModels/TabDemo.java
@@ -2,10 +2,13 @@ package com.gueei.demos.markupDemo.viewModels;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.Toast;
 import com.gueei.demos.markupDemo.OptionsMenu;
 import com.gueei.demos.markupDemo.Parceling;
 import com.gueei.demos.markupDemo.R;
 import com.gueei.demos.markupDemo.custom_actionbar.ActCustomAB;
+import gueei.binding.Command;
 import gueei.binding.collections.ArrayListObservable;
 import gueei.binding.observables.IntegerObservable;
 import gueei.binding.viewAttributes.tabHost.Tab;
@@ -17,9 +20,16 @@ import gueei.binding.viewAttributes.tabHost.Tab;
  */
 public class TabDemo {
 
-	public ArrayListObservable<Tab> TabsArray = new ArrayListObservable<Tab>(Tab.class);
-	public IntegerObservable TabWidth = new IntegerObservable(160);
-    public IntegerObservable SelectedPosition = new IntegerObservable(1);
+	public ArrayListObservable<Tab> TabsArray        = new ArrayListObservable<Tab>(Tab.class);
+	public IntegerObservable        TabWidth         = new IntegerObservable(160);
+	public IntegerObservable        SelectedPosition = new IntegerObservable(2);
+	public Command                  TabChanged       = new Command() {
+		@Override public void Invoke(View view, Object... args) {
+			Toast.makeText(view.getContext(), "Selected tab with tag: " + (String) args[0] + " selected position=" + SelectedPosition.get(),
+						   Toast.LENGTH_SHORT)
+				 .show();
+		}
+	};
 	//
 	private Context mContext;
 
@@ -48,6 +58,5 @@ public class TabDemo {
 		tab.Label.set("Options Menu");
 		tab.Icon.set(drawable);
 		TabsArray.add(tab);
-
 	}
 }

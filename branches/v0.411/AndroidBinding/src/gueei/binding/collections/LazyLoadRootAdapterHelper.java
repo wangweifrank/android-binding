63e3d99d940762eb1780f7b7d5571d70e25f45cc
gueei.com@gmail.com <gueei.com@gmail.com@2838e9db-b119-15c4-5bde-0ef843f85f1f>
2011-09-05 09:33:49 +0000

    Branch to test the newer / better interface of IBindableView

---
+++ branches/v0.411/AndroidBinding/src/gueei/binding/collections/LazyLoadRootAdapterHelper.java
@@ -0,0 +1,51 @@
+package gueei.binding.collections;
+
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
+
+public class LazyLoadRootAdapterHelper implements OnScrollListener {
+	private final AbsListView mView;
+	private final LazyLoadAdapter mAdapter;
+	private LazyLoadAdapter.Mode mMode;
+	private boolean busy;
+	
+	public LazyLoadRootAdapterHelper(AbsListView view, LazyLoadAdapter adapter, LazyLoadAdapter.Mode mode){
+		mAdapter = adapter;
+		view.setOnScrollListener(this);
+		mMode = mode;
+		mView = view;
+	}
+	
+	public void setMode(LazyLoadAdapter.Mode mode){
+		mMode = mode;
+	}
+	
+	private int lastFirstVisibleItem, lastVisibleItemCount;
+	
+	public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+		lastFirstVisibleItem = firstVisibleItem;
+		lastVisibleItemCount = visibleItemCount;
+		if (!busy)
+			mAdapter.onVisibleChildrenChanged(firstVisibleItem, visibleItemCount);
+	}
+
+	public boolean isBusy(){
+		return busy;
+	}
+	
+	int lastScrollState = OnScrollListener.SCROLL_STATE_IDLE;
+	
+	public void onGetView(int position){
+		if (!busy)
+			mAdapter.onVisibleChildrenChanged(mView.getFirstVisiblePosition(), mView.getLastVisiblePosition() - mView.getFirstVisiblePosition());
+	}
+	
+	public void onScrollStateChanged(AbsListView view, int scrollState) {
+		if (mMode.equals(LazyLoadAdapter.Mode.LoadWhenScrolling) 
+				|| scrollState==OnScrollListener.SCROLL_STATE_IDLE){
+			busy = false;
+			mAdapter.onVisibleChildrenChanged(lastFirstVisibleItem, lastVisibleItemCount);
+		}else
+			busy = true;
+	}
+}

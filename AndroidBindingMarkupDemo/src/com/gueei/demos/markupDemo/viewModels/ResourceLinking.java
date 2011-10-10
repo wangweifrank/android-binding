56de2d2efd21eb409ef0064ba9ff9fb8a57fd619
gueei.com <gueei.com@gmail.com>
2011-10-10 07:33:01 +0000

    Try to work on supporting of Inner Field

--- AndroidBindingMarkupDemo/src/com/gueei/demos/markupDemo/viewModels/ResourceLinking.java
+++
@@ -1,8 +0,0 @@
-package com.gueei.demos.markupDemo.viewModels;
-
-import android.view.View;
-import gueei.binding.Command;
-import gueei.binding.observables.StringObservable;
-
-public class ResourceLinking {
-}
--- AndroidBindingV30/src/gueei/binding/v30/app/BindingActivityV30.java
+++
@@ -1,7 +0,0 @@
-package gueei.binding.v30.app;
-
-import gueei.binding.app.BindingActivity;
-
-public class BindingActivityV30 extends BindingActivity {
-
-}

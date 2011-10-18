e40163a919dde3da89428dbee585dcebcabf9097
egandro <egandro@gmail.com>
2011-10-18 13:47:56 +0000

    added a simple wizzard as demo (this will crash in step3)

---
+++ AndroidBindingBindableFrameLayoutDemo/src/com/gueei/demos/bindableframelayoutdemo/viewmodels/Step1.java
@@ -0,0 +1,8 @@
+package com.gueei.demos.bindableframelayoutdemo.viewmodels;
+
+import gueei.binding.observables.StringObservable;
+
+public class Step1 {
+	public final StringObservable Step1Text = new StringObservable("Step1");	
+	public final StringObservable HelloWorld = new StringObservable("hello world1");	
+}

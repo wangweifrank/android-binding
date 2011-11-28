ee61dc81e29bea47358b828b6163e1b66a39310a
gueei.com@gmail.com <gueei.com@gmail.com@2838e9db-b119-15c4-5bde-0ef843f85f1f>
2011-11-28 08:16:06 +0000

    Adding Context to LazyLoadParent

--- branches/fragmentAlternative/AndroidBinding/src/gueei/binding/collections/LazyLoadParent.java
+++ branches/fragmentAlternative/AndroidBinding/src/gueei/binding/collections/LazyLoadParent.java
@@ -1,5 +1,7 @@
 package gueei.binding.collections;
 
+import android.content.Context;
+
 /**
  * Collection with this interface will not load all children
  * immediately, rather they are loaded when required (displaying, for example)
@@ -7,5 +9,9 @@ package gueei.binding.collections;
  *
  */
 public interface LazyLoadParent {
-	public void onLoadChildren();
+	/**
+	 * Changed to include the context. 
+	 * @param context
+	 */
+	public void onLoadChildren(Context context);
 }

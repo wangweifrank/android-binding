529ffdb028242d1a545710196f2b8eca4a3cfbd1
kulynyak@gmail.com <kulynyak@gmail.com@2838e9db-b119-15c4-5bde-0ef843f85f1f>
2011-12-08 10:01:42 +0000

    Fixed issue with crash when Collection is changed(and new size is less than before): Adapter  tries to load item at position greater than new Collection size. Some performance improvement and more accurate Collection bounds handling for CollectionAdapter.onVisibleChildrenChanged() CollectionAdapter + LazyLoadRootAdapter

--- trunk/AndroidBinding/src/gueei/binding/collections/CollectionAdapter.java
+++ trunk/AndroidBinding/src/gueei/binding/collections/CollectionAdapter.java
@@ -1,7 +1,6 @@
 package gueei.binding.collections;
 
 import java.util.ArrayList;
-
 import gueei.binding.AttributeBinder;
 import gueei.binding.Binder;
 import gueei.binding.CollectionObserver;
@@ -19,8 +18,8 @@ import android.widget.BaseAdapter;
 import android.widget.Filter;
 import android.widget.Filterable;
 
-public class CollectionAdapter extends BaseAdapter
-	implements CollectionObserver, Filterable, LazyLoadAdapter{
+public class CollectionAdapter extends BaseAdapter implements CollectionObserver, Filterable, LazyLoadAdapter {
+
 	@Override
 	public int getViewTypeCount() {
 		return mLayout.getTemplateCount();
@@ -38,8 +37,8 @@ public class CollectionAdapter extends BaseAdapter
 	protected final IModelReflector mReflector;
 	protected final Filter mFilter;
 
-	public CollectionAdapter(Context context, IModelReflector reflector,
-			IObservableCollection<?> collection, Layout layout, Layout dropDownLayout, Filter filter) throws Exception{
+	public CollectionAdapter(Context context, IModelReflector reflector, IObservableCollection<?> collection, Layout layout, Layout dropDownLayout,
+			Filter filter) throws Exception {
 		mHandler = new Handler();
 		mContext = context;
 		mLayout = layout;
@@ -49,26 +48,22 @@ public class CollectionAdapter extends BaseAdapter
 		mFilter = filter;
 		mCollection.subscribe(this);
 	}
-	
-	public CollectionAdapter(Context context, IModelReflector reflector,
-			IObservableCollection<?> collection, Layout layout, Layout dropDownLayout) throws Exception{
+
+	public CollectionAdapter(Context context, IModelReflector reflector, IObservableCollection<?> collection, Layout layout, Layout dropDownLayout)
+			throws Exception {
 		this(context, reflector, collection, layout, dropDownLayout, null);
 	}
-	
+
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public CollectionAdapter(Context context, IObservableCollection<?> collection, 
-			Layout layout, Layout dropDownLayout, Filter filter) throws Exception{
-		this(context, 
-				new CachedModelReflector(collection.getComponentType()), collection, layout, dropDownLayout, filter);
+	public CollectionAdapter(Context context, IObservableCollection<?> collection, Layout layout, Layout dropDownLayout, Filter filter) throws Exception {
+		this(context, new CachedModelReflector(collection.getComponentType()), collection, layout, dropDownLayout, filter);
 	}
-	
+
 	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public CollectionAdapter(Context context, IObservableCollection<?> collection, 
-			Layout layout, Layout dropDownLayout) throws Exception{
-		this(context, 
-				new CachedModelReflector(collection.getComponentType()), collection, layout, dropDownLayout);		
+	public CollectionAdapter(Context context, IObservableCollection<?> collection, Layout layout, Layout dropDownLayout) throws Exception {
+		this(context, new CachedModelReflector(collection.getComponentType()), collection, layout, dropDownLayout);
 	}
-	
+
 	public void subscribeCollectionObserver(CollectionObserver observer) {
 		mCollection.subscribe(observer);
 	}
@@ -91,28 +86,27 @@ public class CollectionAdapter extends BaseAdapter
 
 	private View getView(int position, View convertView, ViewGroup parent, int layoutId) {
 		View returnView = convertView;
-		if (position>=mCollection.size()) return returnView;
+		if (position >= mCollection.size())
+			return returnView;
 		try {
 			ObservableMapper mapper;
-			
+
 			mCollection.onLoad(position);
 
 			Object item = mCollection.getItem(position);
-			
-			if (mHelper!=null && !mHelper.isBusy()){
-				if (item instanceof LazyLoadRowModel) 
-					((LazyLoadRowModel)item).display(mCollection, position);
+
+			if (mHelper != null && !mHelper.isBusy()) {
+				if (item instanceof LazyLoadRowModel)
+					((LazyLoadRowModel) item).display(mCollection, position);
 			}
-			
-			if ((convertView == null) || 
-					((mapper = getAttachedMapper(convertView))==null)) {
-				
-				Binder.InflateResult result = Binder.inflateView(mContext,
-						layoutId, parent, false);
+
+			if ((convertView == null) || ((mapper = getAttachedMapper(convertView)) == null)) {
+
+				Binder.InflateResult result = Binder.inflateView(mContext, layoutId, parent, false);
 				mapper = new ObservableMapper();
 				Object model = mCollection.getItem(position);
 				mapper.startCreateMapping(mReflector, model);
-				for(View view: result.processedViews){
+				for (View view : result.processedViews) {
 					AttributeBinder.getInstance().bindView(mContext, view, mapper);
 				}
 				mapper.endCreateMapping();
@@ -126,7 +120,7 @@ public class CollectionAdapter extends BaseAdapter
 			return returnView;
 		}
 	}
-	
+
 	@Override
 	public View getDropDownView(int position, View convertView, ViewGroup parent) {
 		return getView(position, convertView, parent, mDropDownLayout.getLayoutId(position));
@@ -135,18 +129,19 @@ public class CollectionAdapter extends BaseAdapter
 	public View getView(int position, View convertView, ViewGroup parent) {
 		return getView(position, convertView, parent, mLayout.getLayoutId(position));
 	}
-	
-	private ObservableMapper getAttachedMapper(View convertView){
+
+	private ObservableMapper getAttachedMapper(View convertView) {
 		return Binder.getViewTag(convertView).get(ObservableMapper.class);
 	}
-	
-	private void putAttachedMapper(View convertView, ObservableMapper mapper){
+
+	private void putAttachedMapper(View convertView, ObservableMapper mapper) {
 		Binder.getViewTag(convertView).put(ObservableMapper.class, mapper);
 	}
-		
+
 	public void onCollectionChanged(IObservableCollection<?> collection) {
-		mHandler.post(new Runnable(){
-			public void run(){
+		mHandler.post(new Runnable() {
+
+			public void run() {
 				notifyDataSetChanged();
 			}
 		});
@@ -158,75 +153,80 @@ public class CollectionAdapter extends BaseAdapter
 
 	protected Mode mMode = Mode.LoadWhenStopped;
 	protected LazyLoadRootAdapterHelper mHelper;
-	
+
 	public void setRoot(AbsListView view) {
-		if(LazyLoadRowModel.class.isAssignableFrom(mCollection.getComponentType()))
+		if (LazyLoadRowModel.class.isAssignableFrom(mCollection.getComponentType()))
 			mHelper = new LazyLoadRootAdapterHelper(view, this, mMode);
 	}
 
 	public void setMode(Mode mode) {
-		if (mHelper!=null)
-		{
+		if (mHelper != null) {
 			mHelper.setMode(mode);
 		}
 		mMode = mode;
 	}
 
-	private int lastDisplayingFirst = -1, lastTotal = 0;
-	
+	private int lastDisplayingFirst = -1;
+	private int lastTotal = 0;
+
 	public void onVisibleChildrenChanged(int first, int total) {
-		int collectionSizeLocalCache = mCollection.size();
-		total = (collectionSizeLocalCache < total) ? collectionSizeLocalCache : total;
+		int actualCollectionSize = mCollection.size();
+		if (0 == actualCollectionSize) {
+			// nothing to show, nothing to hide, reset last* 
+			lastDisplayingFirst = -1;
+			lastTotal = 0;
+			return;
+		}
+		// normalize NEW first and last indexes
+		// normalize newTotal, should be greater or equal to 0
+		int newTotal = total < 0 ? 0 : total;
+		// normalize newFirstIndex, should be less than actualCollectionSize
+		int newFirstIndex = (first >= actualCollectionSize) ? actualCollectionSize - 1 : first;
+		// normalize newFirstIndex, should be greater or equal to 0
+		newFirstIndex = newFirstIndex < 0 ? 0 : newFirstIndex;
+		// calculate last new visible index
+		int newLastIndex = newFirstIndex + newTotal;
+		// normalize newLastIndex, should be equal or greater than less than newFirstIndex
+		newLastIndex = (newLastIndex < newFirstIndex) ? newFirstIndex + 1 : newLastIndex;
+		// normalize newLastIndex, should be less than actualCollectionSize
+		newLastIndex = (newLastIndex > actualCollectionSize) ? actualCollectionSize : newLastIndex;
 
-		if (lastTotal != total)
-			mCollection.setVisibleChildrenCount(this, total);
-		
-		int nTotal = total; // > total ? lastTotal : total;
-		
-		// keep in bounds
-		if (lastTotal > total) {
-			lastTotal = total;
+		// normalize OLD first and last indexes (collection size can be changed between function calls)
+		// normalize oldTotal, should be greater or equal to 0
+		int oldTotal = lastTotal < 0 ? 0 : lastTotal;
+		// normalize oldFirstIndex, should be less or equal to actualCollectionSize
+		int oldFirstIndex = (lastDisplayingFirst > actualCollectionSize - 1) ? actualCollectionSize - 1 : lastDisplayingFirst;
+		// normalize oldFirstIndex, should be greater or equal to 0
+		oldFirstIndex = oldFirstIndex < 0 ? 0 : oldFirstIndex;
+		// calculate last old visible index
+		int oldLastIndex = oldFirstIndex + oldTotal;
+		// normalize oldLastIndex, should be equal or greater than less than oldFirstIndex
+		oldLastIndex = (oldLastIndex < oldFirstIndex) ? oldFirstIndex + 1 : oldLastIndex;
+		// normalize oldLastIndex, should be less or equal to actualCollectionSize
+		oldLastIndex = (oldLastIndex > actualCollectionSize) ? actualCollectionSize : oldLastIndex;
+
+		LazyLoadRowModel item;
+
+		for (int i = newFirstIndex; i < oldFirstIndex; ++i) {
+			item = (LazyLoadRowModel) mCollection.getItem(i);
+			item.display(mCollection, i);
 		}
-		if (lastDisplayingFirst > total) {
-			lastDisplayingFirst = 0;
+		for (int i = oldFirstIndex; i < newFirstIndex; ++i) {
+			item = (LazyLoadRowModel) mCollection.getItem(i);
+			item.hide(mCollection, i);
 		}
 
-		ArrayList<Integer> lastDisplaying = new ArrayList<Integer>();
-		for(int i=lastDisplayingFirst; i<lastDisplayingFirst+lastTotal; i++){
-			lastDisplaying.add(i);
+		for (int i = newLastIndex; i < oldLastIndex; ++i) {
+			item = (LazyLoadRowModel) mCollection.getItem(i);
+			item.hide(mCollection, i);
 		}
-//		String hide = lastDisplayingFirst + ", " + total + " show: ";
-		
-		for(int i=first; i<first + nTotal; i++){
-			int idx = lastDisplaying.indexOf(i);
-			if (idx>=0){
-				lastDisplaying.remove(idx);
-			}else{
-//				hide += i + ", ";
-				Object item = mCollection.getItem(i);
-				if (item instanceof LazyLoadRowModel)
-					((LazyLoadRowModel)item).display(mCollection, i);
-			}
+		for (int i = oldLastIndex; i < newLastIndex; ++i) {
+			item = (LazyLoadRowModel) mCollection.getItem(i);
+			item.display(mCollection, i);
 		}
 
-		
-//		hide += " hide: ";
-		
-		for(Integer i: lastDisplaying){
-			Object item = mCollection.getItem(i);
-//			hide += i + ", ";
-			if (item instanceof LazyLoadRowModel)
-				((LazyLoadRowModel)item).hide(mCollection, i);
-		}
-		
-//		Log.d("Binder", hide);
-		
-		
-		lastDisplayingFirst = first;
-		lastTotal = total;
-		
-		// Don't want to frequently grow and shrink the total size 
-//		if (lastTotal < total)
-	//		lastTotal = total;
+		// set lastDisplayingFirst and lastTotal;
+		lastDisplayingFirst = newFirstIndex;
+		lastTotal = newTotal;
 	}
 }

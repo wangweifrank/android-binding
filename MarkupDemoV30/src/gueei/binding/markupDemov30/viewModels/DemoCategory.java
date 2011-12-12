260fe2fb5749bcb457fc6646d8fa53271bb601e5
gueei.com <gueei.com@gmail.com>
2011-12-12 05:50:41 +0000

    Skeleton of MarkupDemoV30 is done.

--- MarkupDemoV30/src/gueei/binding/markupDemov30/viewModels/DemoCategory.java
+++ MarkupDemoV30/src/gueei/binding/markupDemov30/viewModels/DemoCategory.java
@@ -1,7 +1,12 @@
 package gueei.binding.markupDemov30.viewModels;
 
+import gueei.binding.Command;
 import gueei.binding.collections.ArrayListObservable;
+import gueei.binding.labs.EventAggregator;
 import gueei.binding.observables.StringObservable;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
 
 public class DemoCategory {
 	public final StringObservable Name = 
@@ -9,8 +14,25 @@ public class DemoCategory {
 	
 	public final ArrayListObservable<DemoEntry> Entries = 
 			new ArrayListObservable<DemoEntry>(DemoEntry.class);
+
+	private Context mContext;
 	
-	public DemoCategory(String name){
+	public DemoCategory(Context context, String name){
 		Name.set(name);
+		mContext = context;
 	}
+	
+	public final Command ShowDemo = new Command(){
+		@Override
+		public void Invoke(View view, Object... args) {
+			if (args.length>0){
+				DemoEntry entry = (DemoEntry)args[0];
+				Bundle bundle = new Bundle();
+				bundle.putString("Clazz", entry.ModelClass.get().getName());
+				bundle.putInt("Layout", entry.LayoutId.get());
+				EventAggregator.getInstance(mContext)
+					.publish("ShowDemo", DemoCategory.this, bundle);
+			}
+		}
+	};
 }
---
+++ MarkupDemoV30/src/gueei/binding/markupDemov30/viewModels/LaunchCodeViewModel.java
@@ -0,0 +1,12 @@
+package gueei.binding.markupDemov30.viewModels;
+
+import gueei.binding.markupDemov30.R;
+
+
+public class LaunchCodeViewModel extends CodeViewModel {
+	public LaunchCodeViewModel() {
+		super();
+		Resource.set(R.raw.launchviewmodel_code);
+		Type.set("java");
+	}
+}

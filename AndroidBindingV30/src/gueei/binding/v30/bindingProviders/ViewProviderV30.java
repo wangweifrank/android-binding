4dadb3f2e71c3bdc51862c29429d9f2681a38bd9
egandro <egandro@gmail.com>
2012-01-04 09:28:41 +0000

    renamed to onAttach, onDetach

--- AndroidBindingV30/src/gueei/binding/v30/bindingProviders/ViewProviderV30.java
+++ AndroidBindingV30/src/gueei/binding/v30/bindingProviders/ViewProviderV30.java
@@ -4,7 +4,8 @@ import android.view.View;
 import gueei.binding.ViewAttribute;
 import gueei.binding.bindingProviders.BindingProvider;
 import gueei.binding.v30.viewAttributes.AnimationViewAttributeV30;
-import gueei.binding.v30.viewAttributes.OnAttachStateChangeListenerAttributeV30;
+import gueei.binding.v30.viewAttributes.OnAttachViewAttributeV30;
+import gueei.binding.v30.viewAttributes.OnDetachViewAttributeV30;
 
 public class ViewProviderV30 extends BindingProvider{
 	@SuppressWarnings("unchecked")
@@ -13,8 +14,10 @@ public class ViewProviderV30 extends BindingProvider{
 			View view, String attributeId) {
 		if (attributeId.equals("animation"))
 			return (ViewAttribute<Tv, ?>) new AnimationViewAttributeV30(view);
-		if (attributeId.equals("onAttachStateChangeListener"))
-			return (ViewAttribute<Tv, ?>) new OnAttachStateChangeListenerAttributeV30(view);		
+		if (attributeId.equals("onAttach"))
+			return (ViewAttribute<Tv, ?>) new OnAttachViewAttributeV30(view);
+		if (attributeId.equals("onDetach"))
+			return (ViewAttribute<Tv, ?>) new OnDetachViewAttributeV30(view);				
 		return null;
 	}
 }
--- AndroidBindingV30/src/gueei/binding/v30/viewAttributes/AttachStateChange.java
+++
@@ -1,6 +0,0 @@
-package gueei.binding.v30.viewAttributes;
-
-public class AttachStateChange {
-	public final static int ATTACHED = 0;
-	public final static int DETACHED = 1;
-}

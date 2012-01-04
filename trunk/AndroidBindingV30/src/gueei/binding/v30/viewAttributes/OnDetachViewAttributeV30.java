5b71088866ba53cd4b77b61719392f0d361f2342
egandro@gmail.com <egandro@gmail.com@2838e9db-b119-15c4-5bde-0ef843f85f1f>
2012-01-04 09:28:41 +0000

    renamed to onAttach, onDetach

--- trunk/AndroidBindingV30/src/gueei/binding/v30/viewAttributes/OnDetachViewAttributeV30.java
+++ trunk/AndroidBindingV30/src/gueei/binding/v30/viewAttributes/OnDetachViewAttributeV30.java
@@ -5,9 +5,9 @@ import gueei.binding.Binder;
 import gueei.binding.v30.listeners.OnAttachStateChangeListenerMulticast;
 import gueei.binding.viewAttributes.ViewEventAttribute;
 
-public class OnAttachStateChangeListenerAttributeV30 extends ViewEventAttribute<View> implements View.OnAttachStateChangeListener {
-	public OnAttachStateChangeListenerAttributeV30(View view) {
-		super(view, "onAttachStateChangeListener");
+public class OnDetachViewAttributeV30 extends ViewEventAttribute<View> implements View.OnAttachStateChangeListener {
+	public OnDetachViewAttributeV30(View view) {
+		super(view, "onDetach");
 	}
 	
 	@Override
@@ -15,12 +15,11 @@ public class OnAttachStateChangeListenerAttributeV30 extends ViewEventAttribute<
 		Binder.getMulticastListenerForView(view, OnAttachStateChangeListenerMulticast.class).register(this);
 	}
 
-	public void onViewAttachedToWindow(View v) {
-		this.invokeCommand(v, AttachStateChange.ATTACHED);		
+	public void onViewAttachedToWindow(View v) {	
 	}
 
 	public void onViewDetachedFromWindow(View v) {
-		this.invokeCommand(v, AttachStateChange.DETACHED);				
+		this.invokeCommand(v);				
 	}
 	
 }

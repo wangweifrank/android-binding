e162adfff9824147f6073617e8436f23b655c731
egandro <egandro@gmail.com>
2012-01-16 13:47:57 +0000

    check if compatibility library is available

--- AndroidBindingV30/src/gueei/binding/v30/BinderV30.java
+++ AndroidBindingV30/src/gueei/binding/v30/BinderV30.java
@@ -2,6 +2,7 @@ package gueei.binding.v30;
 
 import gueei.binding.AttributeBinder;
 import gueei.binding.Binder;
+import gueei.binding.BindingLog;
 import gueei.binding.BindingMap;
 import gueei.binding.v30.bindingProviders.ViewPagerViewProviderV30;
 import gueei.binding.v30.bindingProviders.ViewProviderV30;
@@ -12,10 +13,27 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class BinderV30 extends gueei.binding.Binder {
+	
+	// idea from roboguice
+    protected static final boolean hasCompatibilityLibrarySupport;
+
+    static {
+        boolean found = false;
+        try {
+            Class.forName("android.support.v4.app.Fragment");
+            found = true;
+        } catch( Exception ignored ) {}
+        hasCompatibilityLibrarySupport = found;
+    }
+	
 	public static void init(Application application){
 		// Put v30 providers first
 		AttributeBinder.getInstance().registerProvider(new ViewProviderV30());
-		AttributeBinder.getInstance().registerProvider(new ViewPagerViewProviderV30());
+		if( hasCompatibilityLibrarySupport ) {
+			AttributeBinder.getInstance().registerProvider(new ViewPagerViewProviderV30());
+		} else {
+			BindingLog.warning("BinderV30.init", "android.support.v4.app compatibility library not found");
+		}
 		Binder.init(application);
 	}
 	

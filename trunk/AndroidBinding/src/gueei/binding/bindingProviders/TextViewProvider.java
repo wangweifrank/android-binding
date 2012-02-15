06c3d077ba53deafe681197ea77d1dbec5b02037
egandro@gmail.com <egandro@gmail.com@2838e9db-b119-15c4-5bde-0ef843f85f1f>
2012-02-15 13:07:54 +0000

    made CheckedTextView bindable to checked

--- trunk/AndroidBinding/src/gueei/binding/bindingProviders/TextViewProvider.java
+++ trunk/AndroidBinding/src/gueei/binding/bindingProviders/TextViewProvider.java
@@ -1,12 +1,14 @@
 package gueei.binding.bindingProviders;
 
 import gueei.binding.ViewAttribute;
+import gueei.binding.viewAttributes.textView.CheckedViewAttribute;
 import gueei.binding.viewAttributes.textView.OnTextChangedViewEvent;
 import gueei.binding.viewAttributes.textView.TextColorViewAttribute;
 import gueei.binding.viewAttributes.textView.TextLinesViewAttribute;
 import gueei.binding.viewAttributes.textView.TextViewAttribute;
 import gueei.binding.viewAttributes.textView.TypefaceViewAttribute;
 import android.view.View;
+import android.widget.CheckedTextView;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -16,6 +18,12 @@ public class TextViewProvider extends BindingProvider {
 	@SuppressWarnings("unchecked")
 	@Override
 	public <Tv extends View>ViewAttribute<Tv, ?> createAttributeForView(View view, String attributeId) {
+		if (view instanceof CheckedTextView) {
+			if (attributeId.equals("checked")){
+				CheckedViewAttribute attr = new CheckedViewAttribute((CheckedTextView)view);
+				return (ViewAttribute<Tv, ?>) attr;
+			}		
+		}		
 		if (!(view instanceof TextView)) return null;
 		if (attributeId.equals("text")){
 			TextViewAttribute attr = new TextViewAttribute((TextView)view, "text");
@@ -41,7 +49,10 @@ public class TextViewProvider extends BindingProvider {
 		if (attributeId.equals("typeface")){
 			TypefaceViewAttribute attr = new TypefaceViewAttribute((TextView)view);
 			return (ViewAttribute<Tv, ?>) attr;
-		}		
+		}
+				
+		
+		
 		return null;
 	}
 }

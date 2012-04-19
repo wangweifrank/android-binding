9a5db09ab6d577092c6520b05dbb5e96f0e6caa6
gueei.com <gueei.com@gmail.com>
2012-04-19 09:53:31 +0000

    New implementation on Binding Action Bar, taking advantage of the existed binding resolving mechanism. Still work in progress

--- MarkupDemoV30/gen/gueei/binding/markupDemov30/R.java
+++ MarkupDemoV30/gen/gueei/binding/markupDemov30/R.java
@@ -8,42 +8,36 @@
 package gueei.binding.markupDemov30;
 
 public final class R {
+    public static final class array {
+        public static final int action_list=0x7f080000;
+    }
     public static final class attr {
     }
     public static final class dimen {
-        public static final int titles_size=0x7f070000;
+        public static final int titles_size=0x7f060000;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int about=0x7f090001;
         public static final int demo_listview=0x7f090000;
-        public static final int menugroup=0x7f090003;
-        public static final int showCustomNotification=0x7f090005;
-        public static final int showStandardNotification=0x7f090004;
-        public static final int toggleTheme=0x7f090002;
     }
     public static final class layout {
         public static final int code_view=0x7f030000;
         public static final int demos=0x7f030001;
         public static final int demos_list_item=0x7f030002;
         public static final int main=0x7f030003;
-    }
-    public static final class menu {
-        public static final int group_about=0x7f080000;
-        public static final int main_menu=0x7f080001;
+        public static final int simple_spinner_dropdown_item=0x7f030004;
     }
     public static final class raw {
         public static final int launchviewmodel_code=0x7f050000;
         public static final int main_metadata=0x7f050001;
     }
     public static final class string {
-        public static final int app_name=0x7f060002;
-        public static final int hello=0x7f060001;
-        public static final int title_actionbar=0x7f060004;
-        public static final int title_source=0x7f060003;
-        public static final int validation_required_message=0x7f060000;
+        public static final int app_name=0x7f070001;
+        public static final int hello=0x7f070000;
+        public static final int title_actionbar=0x7f070003;
+        public static final int title_source=0x7f070002;
     }
     public static final class xml {
         public static final int main_metadata=0x7f040000;

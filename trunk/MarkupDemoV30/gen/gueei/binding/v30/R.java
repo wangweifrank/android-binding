a947eea470e8690d880b876a82d93deabc94d8da
gueei.com@gmail.com <gueei.com@gmail.com@2838e9db-b119-15c4-5bde-0ef843f85f1f>
2012-04-22 01:29:45 +0000

    Please ignore r641 until this...

--- trunk/MarkupDemoV30/gen/gueei/binding/v30/R.java
+++ trunk/MarkupDemoV30/gen/gueei/binding/v30/R.java
@@ -20,14 +20,32 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int demo_listview=0x7f090000;
+        public static final int button1=0x7f0a0002;
+        public static final int button2=0x7f0a0003;
+        public static final int button3=0x7f0a0004;
+        public static final int demo_listview=0x7f0a0001;
+        public static final int my_parent_id=0x7f0a0000;
     }
     public static final class layout {
-        public static final int code_view=0x7f030000;
-        public static final int demos=0x7f030001;
-        public static final int demos_list_item=0x7f030002;
-        public static final int main=0x7f030003;
-        public static final int simple_spinner_dropdown_item=0x7f030004;
+        public static final int bindableframelayout=0x7f030000;
+        public static final int bindableframelayout_frame1=0x7f030001;
+        public static final int bindableframelayout_frame2=0x7f030002;
+        public static final int bindableframelayout_frame3=0x7f030003;
+        public static final int bindableframelayout_frame_on_load=0x7f030004;
+        public static final int bindablelinearlayout=0x7f030005;
+        public static final int bindablelinearlayout_item=0x7f030006;
+        public static final int bindablelinearlayout_item_blue=0x7f030007;
+        public static final int bindabletablelayout=0x7f030008;
+        public static final int bindabletablelayout_item=0x7f030009;
+        public static final int bindabletablelayout_item_blue=0x7f03000a;
+        public static final int code_view=0x7f03000b;
+        public static final int demos=0x7f03000c;
+        public static final int demos_list_item=0x7f03000d;
+        public static final int main=0x7f03000e;
+        public static final int simple_spinner_dropdown_item=0x7f03000f;
+    }
+    public static final class menu {
+        public static final int optionsmenu=0x7f090000;
     }
     public static final class raw {
         public static final int launchviewmodel_code=0x7f050000;
@@ -40,6 +58,7 @@ public final class R {
         public static final int title_source=0x7f070002;
     }
     public static final class xml {
-        public static final int main_metadata=0x7f040000;
+        public static final int demos=0x7f040000;
+        public static final int main_metadata=0x7f040001;
     }
 }

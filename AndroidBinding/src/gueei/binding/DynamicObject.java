dff3746d0e5a2b03c016e176011cb6facd26ca18
gueei.com <gueei.com@gmail.com>
2012-04-29 02:30:30 +0000

    Bug fixes in DynamicObject (now subscribe to its child) and binding syntaxes (handles better double quote)

--- AndroidBinding/src/gueei/binding/DynamicObject.java
+++ AndroidBinding/src/gueei/binding/DynamicObject.java
@@ -1,17 +1,19 @@
 package gueei.binding;
 
-import java.util.WeakHashMap;
+import java.util.Collection;
+import java.util.HashMap;
 
-public class DynamicObject extends Observable<DynamicObject> implements IPropertyContainer{
+public class DynamicObject extends Observable<DynamicObject> implements IPropertyContainer, Observer{
 	public DynamicObject() {
 		super(DynamicObject.class);
 	}
 
-	private WeakHashMap<String, IObservable<?>> observables = 
-		 new WeakHashMap<String, IObservable<?>>();
+	private HashMap<String, IObservable<?>> observables = 
+		 new HashMap<String, IObservable<?>>();
 	
 	public void putObservable(String name, IObservable<?> observable){
 		observables.put(name, observable);
+		observable.subscribe(this);
 	}
 	
 	public boolean observableExists(String name){
@@ -33,5 +35,11 @@ public class DynamicObject extends Observable<DynamicObject> implements IPropert
 
 	public Object getValueByName(String name) throws Exception {
 		return null;
+	}
+
+	@Override
+	public void onPropertyChanged(IObservable<?> prop,
+			Collection<Object> initiators) {
+		this.notifyChanged(initiators);
 	}
 }

0f4f1874ebf879088f278b10c82b4057f3656382
egandro@gmail.com <egandro@gmail.com@2838e9db-b119-15c4-5bde-0ef843f85f1f>
2012-07-25 12:20:27 +0000

    added log cat tool to output the memory consumption

--- Test/trunk/MemoryLeak/src/com/gueei/memoryleak/MainActivity.java
+++ Test/trunk/MemoryLeak/src/com/gueei/memoryleak/MainActivity.java
@@ -4,7 +4,10 @@ import gueei.binding.Command;
 import gueei.binding.v30.app.BindingActivityV30;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends BindingActivityV30 {
 
@@ -13,9 +16,41 @@ public class MainActivity extends BindingActivityV30 {
         super.onCreate(savedInstanceState);
         setAndBindRootView(R.layout.activity_main,this);
         setAndBindOptionsMenu(R.menu.activity_main, this);
+        showRAM();
     }
+        
+    private void showRAM() {    	
+    	long max = Runtime.getRuntime().maxMemory();
+    	long heapSize = Runtime.getRuntime().totalMemory();
+    	long freeSize = Runtime.getRuntime().freeMemory();
+    	long allocSize = heapSize - freeSize;
+    	
+    	
+    	StringBuilder sb = new StringBuilder();
+    	
+    	sb.append("max: ");
+    	sb.append(max / 1024L);
+    	sb.append("kb ");
+    	sb.append("heap: ");
+    	sb.append(heapSize / 1024L);
+    	sb.append("kb ");
+    	sb.append("alloc: ");
+    	sb.append(allocSize / 1024L);
+    	sb.append("kb ");
+    	sb.append("free: ");
+    	sb.append(freeSize / 1024L);
+    	sb.append("kb ");
+    	
+		Log.v("showRAM", sb.toString());
+	}
 
-    public final Command OnClickWithBinding = new Command() {
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		showRAM();
+	}
+
+	public final Command OnClickWithBinding = new Command() {
 		
  		@Override
  		public void Invoke(View arg0, Object... arg1) {

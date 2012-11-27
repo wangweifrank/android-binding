fbd65f6d15dabb804624ac4345e48873d5c3c6a7
egandro@gmail.com <egandro@gmail.com@2838e9db-b119-15c4-5bde-0ef843f85f1f>
2012-11-27 19:07:25 +0000

    added animationdrawable, added .view package

---
+++ trunk/AndroidBindingV30/src/gueei/binding/v30/viewAttributes/view/BackgroundViewAttributeV30.java
@@ -0,0 +1,47 @@
+package gueei.binding.v30.viewAttributes.view;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import gueei.binding.BindingType;
+import gueei.binding.ViewAttribute;
+
+public class BackgroundViewAttributeV30  extends ViewAttribute<View, Object> {
+	
+	public BackgroundViewAttributeV30(View view) {
+		super(Object.class, view, "backgroundv30");
+	}
+
+	@Override
+	protected void doSetAttributeValue(Object newValue) {
+		if(getView()==null) return;
+		if (newValue==null){
+			getView().setBackgroundDrawable(null);
+			return;
+		}
+		if (newValue instanceof Integer){
+			getView().setBackgroundResource((Integer)newValue);
+		}
+		if (newValue instanceof AnimationDrawable){
+			getView().setBackgroundDrawable((AnimationDrawable)newValue);			
+			AnimationDrawable frameAnimation = (AnimationDrawable)newValue;
+			frameAnimation.start();			
+			return;
+		}
+		if (newValue instanceof Drawable){
+			getView().setBackgroundDrawable((Drawable)newValue);
+			return;
+		}
+	}
+
+	@Override
+	protected BindingType AcceptThisTypeAs(Class<?> type) {
+		return BindingType.OneWay;
+	}
+
+	@Override
+	public Object get() {
+		return null;
+	}
+
+}
